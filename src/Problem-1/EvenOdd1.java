68f5d6ab2bbe21a269248d7deea7ea4ef1cddc37
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-06-08 00:42:07 +0600

    [solve](problem-1): even odd problem solved

---
+++ src/Problem-1/EvenOdd1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class EvenOdd1 {
+
+    public static void main(String[] args) {
+        int T, i, n;
+        Scanner scanner = new Scanner(System.in);
+
+        T = scanner.nextInt();
+
+        for (i = 1; i <= T; i++) {
+
+            n = scanner.nextInt();
+
+            if (n % 2 == 0) {
+                System.out.println("even");
+            } else {
+                System.out.println("odd");
+            }
+        }
+        scanner.close();
+        ;
+
+    }
+
+}
---
+++ src/Problem-2/EvenOdd2.java
@@ -0,0 +1,5 @@
+
+
+public class EvenOdd2 {
+   public sta
+}
